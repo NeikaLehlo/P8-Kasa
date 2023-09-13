4cb07a2dc06148b747652d108bd03e0e98bd2743
Chloé Lemonnier <lemoloe@gmail.com>
2023-09-13 17:06:41 +0200

    Add responsive for Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -13,16 +13,18 @@ function Card() {
 
 
     return (
-        <div className="kasa-cards">
-           {lodgingsData.map((lodging)=>(   
-            <article className="kasa-cards-article" key={lodging.id}>
-                <Link className="kasa-cards-link" to={`/lodging/${lodging.id}`}>
-                    <img className="kasa-cards-img" src={lodging.cover} alt="lodging's picture" />
-                    <h2 className="kasa-cards-title">{lodging.title}</h2>
-                </Link>
-            </article>
-        ))}
-        </div>
+        <section className="kasa-cards">
+            <div className="kasa-cards-div">
+            {lodgingsData.map((lodging)=>(   
+                <article className="kasa-cards-article" key={lodging.id}>
+                    <Link className="kasa-cards-link" to={`/lodging/${lodging.id}`}>
+                        <img className="kasa-cards-img" src={lodging.cover} alt="lodging's picture" />
+                        <h2 className="kasa-cards-title">{lodging.title}</h2>
+                    </Link>
+                </article>
+            ))}
+            </div>
+        </section>
     )
 }
 
